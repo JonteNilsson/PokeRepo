dc572398f027b405baf816ef2e5ed73590a7f28a
JonhannesNilsson <jonte9203@hotmail.com>
2024-02-07 11:10:21 +0100

    forgot to save

--- PokeRepo/Models/Pokemon.cs
+++ PokeRepo/Models/Pokemon.cs
@@ -2,5 +2,10 @@
 {
     public class Pokemon
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Type { get; set; } = null!;
+
+
     }
 }
